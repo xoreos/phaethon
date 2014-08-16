a1e5bc30ffb818cd471b2c08bd1b2c8b290e2683
Sven Hesse <drmccoy@drmccoy.de>
2014-08-16 21:28:25 +0200

    COMMON: Add StackException/UString/Stream code
    Copied from xoreos/ScummVM

---
+++ src/common/error.cpp
@@ -0,0 +1,116 @@
+/* Phaethon - A FLOSS resource explorer for BioWare's Aurora engine games
+ *
+ * Phaethon is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Phaethon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Phaethon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phaethon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file common/error.cpp
+ *  Basic exceptions to throw.
+ */
+
+#include <cstdarg>
+#include <cstdio>
+
+#include "common/error.h"
+#include "common/util.h"
+
+namespace Common {
+
+StackException::StackException() {
+}
+
+StackException::StackException(const char *s, ...) {
+	char buf[STRINGBUFLEN];
+	va_list va;
+
+	va_start(va, s);
+	vsnprintf(buf, STRINGBUFLEN, s, va);
+	va_end(va);
+
+	_stack.push(buf);
+}
+
+StackException::StackException(const StackException &e) : _stack(e._stack) {
+}
+
+StackException::StackException(const std::exception &e) {
+	add(e);
+}
+
+StackException::~StackException() throw() {
+}
+
+void StackException::add(const char *s, ...) {
+	char buf[STRINGBUFLEN];
+	va_list va;
+
+	va_start(va, s);
+	vsnprintf(buf, STRINGBUFLEN, s, va);
+	va_end(va);
+
+	_stack.push(buf);
+}
+
+void StackException::add(const std::exception &e) {
+	add("%s", e.what());
+}
+
+const char *StackException::what() const throw() {
+	if (_stack.empty())
+		return "";
+
+	return _stack.top().c_str();
+}
+
+bool StackException::isEmpty() const {
+	return _stack.empty();
+}
+
+StackException::Stack &StackException::getStack() {
+	return _stack;
+}
+
+
+const Exception kOpenError("Can't open file");
+const Exception kReadError("Read error");
+const Exception kSeekError("Seek error");
+const Exception kWriteError("Write error");
+
+
+void printException(Exception &e, const UString &prefix) {
+	Exception::Stack &stack = e.getStack();
+
+	try {
+		if (stack.empty()) {
+			status("FATAL ERROR");
+			return;
+		}
+
+		status("%s%s", prefix.c_str(), stack.top().c_str());
+
+		stack.pop();
+
+		while (!stack.empty()) {
+			status("    Because: %s", stack.top().c_str());
+			stack.pop();
+		}
+	} catch (...) {
+		status("FATAL ERROR: Exception while printing exception stack");
+	}
+}
+
+} // End of namespace Common
---
+++ src/common/error.h
@@ -0,0 +1,70 @@
+/* Phaethon - A FLOSS resource explorer for BioWare's Aurora engine games
+ *
+ * Phaethon is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Phaethon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Phaethon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phaethon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file common/error.h
+ *  Basic exceptions to throw.
+ */
+
+#ifndef COMMON_ERROR_H
+#define COMMON_ERROR_H
+
+#include <stack>
+#include <exception>
+
+#include "common/ustring.h"
+
+namespace Common {
+
+/** Exeption that provides a stack of explanations. */
+class StackException : public std::exception {
+public:
+	typedef std::stack<UString> Stack;
+
+	StackException();
+	StackException(const char *s, ...);
+	StackException(const StackException &e);
+	StackException(const std::exception &e);
+	~StackException() throw();
+
+	void add(const char *s, ...);
+	void add(const std::exception &e);
+
+	const char *what() const throw();
+
+	bool isEmpty() const;
+
+	Stack &getStack();
+
+private:
+	Stack _stack;
+};
+
+typedef StackException Exception;
+
+extern const Exception kOpenError;
+extern const Exception kReadError;
+extern const Exception kSeekError;
+extern const Exception kWriteError;
+
+void printException(Exception &e, const UString &prefix = "ERROR: ");
+
+} // End of namespace Common
+
+#endif // COMMON_ERROR_H
