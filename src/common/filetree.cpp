63d297b6010d444e3cc3159128e4dc506b6a03aa
Sven Hesse <drmccoy@drmccoy.de>
2014-08-30 13:06:29 +0200

    COMMON: Add a FileTree

---
+++ src/common/filetree.cpp
@@ -0,0 +1,101 @@
+/* Phaethon - A FLOSS resource explorer for BioWare's Aurora engine games
+ *
+ * Phaethon is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Phaethon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Phaethon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phaethon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file common/filetree.cpp
+ *  A tree structure of files in directories.
+ */
+
+#include "common/filetree.h"
+#include "common/filepath.h"
+
+namespace Common {
+
+FileTree::Entry::Entry() {
+}
+
+FileTree::Entry::Entry(const boost::filesystem::path &p) : name(p.filename().c_str()), path(p) {
+}
+
+
+FileTree::FileTree() {
+}
+
+FileTree::~FileTree() {
+}
+
+void FileTree::clear() {
+	_root.name.clear();
+	_root.path.clear();
+	_root.children.clear();
+}
+
+bool FileTree::isEmpty() const {
+	return _root.name.empty() && _root.path.empty();
+}
+
+const FileTree::Entry &FileTree::getRoot() const {
+	return _root;
+}
+
+bool FileTree::readPath(const Common::UString &path, int recurseDepth) {
+	return readPath(boost::filesystem::path(path.c_str()), recurseDepth);
+}
+
+bool FileTree::readPath(boost::filesystem::path path, int recurseDepth) {
+	clear();
+
+	// The path needs to exist
+	if (!boost::filesystem::exists(path))
+		return false;
+
+	path = FilePath::normalize(path);
+
+	_root.name = path.filename().c_str();
+	_root.path = path;
+
+	// If we can't or shouldn't recurse, we're done
+	if (boost::filesystem::is_regular_file(path) || (recurseDepth == 0))
+		return true;
+
+	return addPath(_root, path, (recurseDepth == -1) ? -1 : (recurseDepth - 1));
+}
+
+bool FileTree::addPath(Entry &entry, const boost::filesystem::path &path, int recurseDepth) {
+	try {
+		// Iterator over the directory's contents
+		boost::filesystem::directory_iterator itEnd;
+		for (boost::filesystem::directory_iterator itDir(path); itDir != itEnd; ++itDir) {
+			// Add the file/directory to the entry's children
+			entry.children.push_back(Entry(itDir->path()));
+
+			// Recurse into directory until the depth limit is reached
+			if (is_directory(itDir->status()))
+				if (recurseDepth != 0)
+					if (!addPath(entry.children.back(), itDir->path(), (recurseDepth == -1) ? -1 : (recurseDepth - 1)))
+						return false;
+		}
+	} catch (...) {
+		return false;
+	}
+
+	return true;
+}
+
+} // End of namespace Common
---
+++ src/common/filetree.h
@@ -0,0 +1,93 @@
+/* Phaethon - A FLOSS resource explorer for BioWare's Aurora engine games
+ *
+ * Phaethon is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Phaethon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Phaethon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phaethon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file common/filetree.h
+ *  A tree structure of files in directories.
+ */
+
+#ifndef COMMON_FILETREE_H
+#define COMMON_FILETREE_H
+
+#include <boost/filesystem.hpp>
+
+#include <list>
+
+#include "common/ustring.h"
+
+namespace Common {
+
+/** A tree structure of files in directories. */
+class FileTree {
+public:
+	/** An entry in the file tree. */
+	struct Entry {
+		/** The name of a file or directory. */
+		UString name;
+		/** The full normalized path of the file or directory. */
+		boost::filesystem::path path;
+
+		/** The files and directories inside this directory entry. */
+		std::list<Entry> children;
+
+		Entry();
+		Entry(const boost::filesystem::path &p);
+	};
+
+	FileTree();
+	~FileTree();
+
+	/** Clear the tree. */
+	void clear();
+
+	/** Is the tree empty? */
+	bool isEmpty() const;
+
+	/** Return the root node. */
+	const Entry &getRoot() const;
+
+	/** Fill the tree with this path.
+	 *
+	 *  @param  p The path to read.
+	 *  @param  recurseDepth The number of levels to recurse into subdirectories.
+	 *                       If 0, only one entry, this path, is added.
+	 *                       If -1, the recursion is limitless.
+	 *  @return true if the path was successfully read, false otherwise.
+	 */
+	bool readPath(boost::filesystem::path path, int recurseDepth = 0);
+
+	/** Fill the tree with this path.
+	 *
+	 *  @param  p The path to read.
+	 *  @param  recurseDepth The number of levels to recurse into subdirectories.
+	 *                       If 0, only one entry, this path, is added.
+	 *                       If -1, the recursion is limitless.
+	 *  @return true if the path was successfully read, false otherwise.
+	 */
+	bool readPath(const Common::UString &path, int recurseDepth = 0);
+
+private:
+	Entry _root;
+
+	bool addPath(Entry &entry, const boost::filesystem::path &path, int recurseDepth);
+};
+
+} // End of namespace Common
+
+#endif // COMMON_FILETREE_H
