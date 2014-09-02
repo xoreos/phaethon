a25575d37a2fca28a7b51d87ae4026cfa5321360
Sven Hesse <drmccoy@drmccoy.de>
2014-09-02 16:42:31 +0200

    AURORA: Add ZipFile from xoreos

---
+++ src/aurora/zipfile.cpp
@@ -0,0 +1,76 @@
+/* Phaethon - A FLOSS resource explorer for BioWare's Aurora engine games
+ *
+ * Phaethon is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Phaethon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Phaethon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phaethon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file aurora/zipfile.cpp
+ *  A ZIP archive.
+ */
+
+#include "common/ustring.h"
+#include "common/file.h"
+#include "common/zipfile.h"
+#include "common/filepath.h"
+
+#include "aurora/zipfile.h"
+#include "aurora/util.h"
+
+namespace Aurora {
+
+ZIPFile::ZIPFile(const Common::UString &fileName) : _zipFile(0) {
+	_zipFile = new Common::ZipFile(fileName);
+
+	load();
+}
+
+ZIPFile::~ZIPFile() {
+	delete _zipFile;
+}
+
+void ZIPFile::clear() {
+	_resources.clear();
+}
+
+const Archive::ResourceList &ZIPFile::getResources() const {
+	return _resources;
+}
+
+uint32 ZIPFile::getResourceSize(uint32 index) const {
+	return _zipFile->getFileSize(index);
+}
+
+Common::SeekableReadStream *ZIPFile::getResource(uint32 index) const {
+	return _zipFile->getFile(index);
+}
+
+void ZIPFile::load() {
+	const Common::ZipFile::FileList &files = _zipFile->getFiles();
+	for (Common::ZipFile::FileList::const_iterator file = files.begin(); file != files.end(); ++file) {
+		Resource res;
+
+		res.name  = Common::FilePath::getStem(file->name);
+		res.type  = TypeMan.getFileType(file->name);
+		res.index = file->index;
+
+		_resources.push_back(res);
+	}
+
+	_zipFile->clear();
+}
+
+} // End of namespace Aurora
---
+++ src/aurora/zipfile.h
@@ -0,0 +1,74 @@
+/* Phaethon - A FLOSS resource explorer for BioWare's Aurora engine games
+ *
+ * Phaethon is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Phaethon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Phaethon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phaethon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file aurora/zipfile.h
+ *  A ZIP archive.
+ */
+
+#ifndef AURORA_ZIPFILE_H
+#define AURORA_ZIPFILE_H
+
+#include <vector>
+
+#include "common/types.h"
+
+#include "aurora/types.h"
+#include "aurora/archive.h"
+
+namespace Common {
+	class UString;
+	class SeekableReadStream;
+	class File;
+	class ZipFile;
+}
+
+namespace Aurora {
+
+/** A class encapsulating ZIP files for resource archive access. */
+class ZIPFile : public Archive {
+public:
+	ZIPFile(const Common::UString &fileName);
+	~ZIPFile();
+
+	/** Clear the resource list. */
+	void clear();
+
+	/** Return the list of resources. */
+	const ResourceList &getResources() const;
+
+	/** Return the size of a resource. */
+	uint32 getResourceSize(uint32 index) const;
+
+	/** Return a stream of the resource's contents. */
+	Common::SeekableReadStream *getResource(uint32 index) const;
+
+private:
+	/** The actual zip file. */
+	Common::ZipFile *_zipFile;
+
+	/** External list of resource names and types. */
+	ResourceList _resources;
+
+	void load();
+};
+
+} // End of namespace Aurora
+
+#endif // AURORA_ZIPFILE_H
