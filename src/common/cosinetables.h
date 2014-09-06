366a8004c97da166402f7725c89ba9995aba8a51
Sven Hesse <drmccoy@drmccoy.de>
2014-09-06 22:50:28 +0200

    COMMON: Add static cosine tables from xoreos

---
+++ src/common/cosinetables.h
@@ -0,0 +1,34 @@
+/* Phaethon - A FLOSS resource explorer for BioWare's Aurora engine games
+ *
+ * Phaethon is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Phaethon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Phaethon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phaethon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file common/cosinetables.h
+ *  Static cosine tables.
+ */
+
+#ifndef COMMON_COSINETABLES_H
+#define COMMON_COSINETABLES_H
+
+namespace Common {
+
+const float *getCosineTable(int bits);
+
+} // End of namespace Common
+
+#endif // COMMON_COSINETABLES_H
