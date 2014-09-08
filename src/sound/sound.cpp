3ac35ea98e36dd351d516687592d3a6e257aeba4
Sven Hesse <drmccoy@drmccoy.de>
2014-09-09 01:03:33 +0200

    SOUND: Add OpenAL SoundManager code from xoreos

--- src/sound/sound.cpp
+++ src/sound/sound.cpp
@@ -35,12 +35,151 @@
 
 DECLARE_SINGLETON(Sound::SoundManager)
 
+/** Control how many buffers per sound OpenAL will create.
+ *
+ *  @note clone2727 says: 5 is just a safe number. Mine only reached a max of 2.
+ */
+static const int kOpenALBufferCount = 5;
+
+/** Number of bytes per OpenAL buffer.
+ *
+ *  @note Needs to be high enough to prevent stuttering, but low enough to
+ *        prevent a noticable lag. 32768 seems to work just fine.
+ */
+static const int kOpenALBufferSize = 32768;
+
 namespace Sound {
 
-SoundManager::SoundManager() {
+SoundManager::SoundManager() : _ready(false), _hasSound(false), _hasMultiChannel(false), _format51(0) {
 }
 
 SoundManager::~SoundManager() {
+}
+
+void SoundManager::init() {
+	for (int i = 0; i < kChannelCount; i++)
+		_channels[i] = 0;
+
+	for (int i = 0; i < kSoundTypeMAX; i++)
+		_types[i].gain = 1.0;
+
+	_curChannel = 1;
+	_curID      = 1;
+
+	_dev = alcOpenDevice(0);
+
+	_hasSound = _dev != 0;
+	if (!_hasSound)
+		warning("Failed to open OpenAL device. Disabling sound output");
+
+	_ctx = 0;
+
+	_hasMultiChannel = false;
+	_format51        = 0;
+
+	if (_hasSound) {
+		_ctx = alcCreateContext(_dev, 0);
+		alcMakeContextCurrent(_ctx);
+		if (!_ctx)
+			throw Common::Exception("Could not create OpenAL context");
+
+		_hasMultiChannel = alIsExtensionPresent("AL_EXT_MCFORMATS");
+		_format51        = alGetEnumValue("AL_FORMAT_51CHN16");
+	}
+
+	createThread();
+
+	_ready = true;
+
+	if (!_hasSound)
+		return;
+
+	setListenerGain(1.0);
+	for (int i = 0; i < kSoundTypeMAX; i++)
+		setTypeGain((SoundType) i, 1.0);
+}
+
+void SoundManager::deinit() {
+	if (!_ready)
+		return;
+
+	destroyThread();
+
+	for (uint16 i = 1; i < kChannelCount; i++)
+		freeChannel(i);
+
+	if (_hasSound) {
+		alcMakeContextCurrent(0);
+		alcDestroyContext(_ctx);
+		alcCloseDevice(_dev);
+	}
+
+	_ready = false;
+}
+
+bool SoundManager::ready() const {
+	return _ready;
+}
+
+void SoundManager::triggerUpdate() {
+	checkReady();
+
+	_needUpdate.signal();
+}
+
+bool SoundManager::isValidChannel(const ChannelHandle &handle) const {
+	if ((handle.channel == 0) || (handle.id == 0) || !_channels[handle.channel])
+		return false;
+
+	if (_channels[handle.channel]->id != handle.id)
+		return false;
+
+	return true;
+}
+
+bool SoundManager::isPlaying(const ChannelHandle &handle) {
+	Common::StackLock lock(_mutex);
+
+	if ((handle.channel == 0) || (handle.id == 0) || !_channels[handle.channel])
+		return false;
+
+	if (_channels[handle.channel]->id != handle.id)
+		return false;
+
+	return isPlaying(handle.channel);
+}
+
+bool SoundManager::isPlaying(uint16 channel) const {
+	if ((channel == 0) || !_channels[channel])
+		return false;
+
+	// TODO: This might pose a problem should we ever need to wait
+	//       for sounds to finish (for syncing, ...). We need to
+	//       add a way for audio streams to tell us how long they are
+	//       and then check if that time has elapsed.
+	if (!_hasSound)
+		return true;
+
+	ALint val;
+	alGetSourcei(_channels[channel]->source, AL_SOURCE_STATE, &val);
+
+	if (val != AL_PLAYING) {
+		if (!_channels[channel]->stream || _channels[channel]->stream->endOfStream()) {
+			ALint buffersQueued, buffersProcessed;
+			alGetSourcei(_channels[channel]->source, AL_BUFFERS_QUEUED,    &buffersQueued);
+			alGetSourcei(_channels[channel]->source, AL_BUFFERS_PROCESSED, &buffersProcessed);
+
+			if (buffersQueued == buffersProcessed)
+				return false;
+		}
+
+		if (_channels[channel]->state != AL_PLAYING)
+			return true;
+
+		alSourcePlay(_channels[channel]->source);
+	}
+
+	return true;
 }
 
 AudioStream *SoundManager::makeAudioStream(Common::SeekableReadStream *stream) {
@@ -119,6 +258,462 @@ AudioStream *SoundManager::makeAudioStream(Common::SeekableReadStream *stream) {
 		return makeMP3Stream(stream, true);
 
 	return makeWAVStream(stream, true);
+}
+
+ChannelHandle SoundManager::playAudioStream(AudioStream *audStream, SoundType type, bool disposeAfterUse) {
+	assert((type >= 0) && (type < kSoundTypeMAX));
+
+	checkReady();
+
+	if (!audStream)
+		throw Common::Exception("No audio stream");
+
+	Common::StackLock lock(_mutex);
+
+	ChannelHandle handle = newChannel();
+
+	_channels[handle.channel] = new Channel;
+	Channel &channel = *_channels[handle.channel];
+
+	channel.id              = handle.id;
+	channel.state           = AL_PAUSED;
+	channel.stream          = audStream;
+	channel.source          = 0;
+	channel.disposeAfterUse = disposeAfterUse;
+	channel.type            = type;
+	channel.typeIt          = _types[channel.type].list.end();
+	channel.gain            = 1.0;
+
+	try {
+
+		if (!channel.stream)
+			throw Common::Exception("Could not detect stream type");
+
+		ALenum error = AL_NO_ERROR;
+
+		if (_hasSound) {
+			// Create the source
+			alGenSources(1, &channel.source);
+			if ((error = alGetError()) != AL_NO_ERROR)
+				throw Common::Exception("OpenAL error while generating sources: %X", error);
+
+			// Create all needed buffers
+			for (int i = 0; i < kOpenALBufferCount; i++) {
+				ALuint buffer;
+
+				alGenBuffers(1, &buffer);
+				if ((error = alGetError()) != AL_NO_ERROR)
+					throw Common::Exception("OpenAL error while generating buffers: %X", error);
+
+				if (fillBuffer(channel.source, buffer, channel.stream)) {
+					// If we could fill the buffer with data, queue it
+
+					alSourceQueueBuffers(channel.source, 1, &buffer);
+					if ((error = alGetError()) != AL_NO_ERROR)
+						throw Common::Exception("OpenAL error while queueing buffers: %X", error);
+
+				} else
+					// If not, put it into our free list
+					channel.freeBuffers.push_back(buffer);
+
+				channel.buffers.push_back(buffer);
+			}
+
+			// Set the gain to the current sound type gain
+			alSourcef(channel.source, AL_GAIN, _types[channel.type].gain);
+		}
+
+		// Add the channel to the correct type list
+		_types[channel.type].list.push_back(&channel);
+		channel.typeIt = --_types[channel.type].list.end();
+
+	} catch (...) {
+		freeChannel(handle);
+		throw;
+	}
+
+	return handle;
+}
+
+ChannelHandle SoundManager::playSoundFile(Common::SeekableReadStream *stream, SoundType type, bool loop) {
+	checkReady();
+
+	if (!stream)
+		throw Common::Exception("No stream");
+
+	AudioStream *audioStream = makeAudioStream(stream);
+
+	if (loop) {
+		RewindableAudioStream *reAudStream = dynamic_cast<RewindableAudioStream *>(audioStream);
+		if (!reAudStream)
+			warning("SoundManager::playSoundFile(): The input stream cannot be rewound, this will not loop.");
+		else
+			audioStream = new LoopingAudioStream(reAudStream, 0);
+	}
+
+	return playAudioStream(audioStream, type);
+}
+
+SoundManager::Channel *SoundManager::getChannel(const ChannelHandle &handle) {
+	if ((handle.channel == 0) || (handle.id == 0))
+		return 0;
+
+	if (!_channels[handle.channel])
+		return 0;
+
+	if (_channels[handle.channel]->id != handle.id)
+		return 0;
+
+	return _channels[handle.channel];
+}
+
+void SoundManager::startChannel(ChannelHandle &handle) {
+	Common::StackLock lock(_mutex);
+
+	Channel *channel = getChannel(handle);
+	if (!channel || !channel->stream)
+		throw Common::Exception("Invalid channel");
+
+	channel->state = AL_PLAYING;
+
+	triggerUpdate();
+}
+
+void SoundManager::pauseChannel(ChannelHandle &handle) {
+	Common::StackLock lock(_mutex);
+
+	Channel *channel = getChannel(handle);
+	if (!channel || !channel->stream)
+		throw Common::Exception("Invalid channel");
+
+	pauseChannel(channel);
+}
+
+void SoundManager::pauseChannel(ChannelHandle &handle, bool pause) {
+	Common::StackLock lock(_mutex);
+
+	Channel *channel = getChannel(handle);
+	if (!channel || !channel->stream)
+		throw Common::Exception("Invalid channel");
+
+	pauseChannel(channel, pause);
+}
+
+void SoundManager::stopChannel(ChannelHandle &handle) {
+	Common::StackLock lock(_mutex);
+
+	freeChannel(handle);
+}
+
+void SoundManager::pauseAll(bool pause) {
+	Common::StackLock lock(_mutex);
+
+	for (uint16 i = 1; i < kChannelCount; i++)
+		pauseChannel(_channels[i], pause);
+}
+
+void SoundManager::stopAll() {
+	Common::StackLock lock(_mutex);
+
+	for (uint16 i = 1; i < kChannelCount; i++)
+		freeChannel(i);
+}
+
+void SoundManager::setListenerGain(float gain) {
+	checkReady();
+
+	Common::StackLock lock(_mutex);
+
+	if (_hasSound)
+		alListenerf(AL_GAIN, gain);
+}
+
+void SoundManager::setChannelPosition(const ChannelHandle &handle, float x, float y, float z) {
+	Common::StackLock lock(_mutex);
+
+	Channel *channel = getChannel(handle);
+	if (!channel || !channel->stream)
+		throw Common::Exception("Invalid channel");
+
+	if (channel->stream->getChannels() > 1)
+		throw Common::Exception("Cannot set position of a non-mono sound.");
+
+	if (_hasSound)
+		alSource3f(channel->source, AL_POSITION, x, y, z);
+}
+
+void SoundManager::getChannelPosition(const ChannelHandle &handle, float &x, float &y, float &z) {
+	Common::StackLock lock(_mutex);
+
+	Channel *channel = getChannel(handle);
+	if (!channel || !channel->stream)
+		throw Common::Exception("Invalid channel");
+
+	if (channel->stream->getChannels() > 1)
+		throw Common::Exception("Cannot get position of a non-mono sound.");
+
+	if (_hasSound)
+		alGetSource3f(channel->source, AL_POSITION, &x, &y, &z);
+}
+
+void SoundManager::setChannelGain(const ChannelHandle &handle, float gain) {
+	Common::StackLock lock(_mutex);
+
+	Channel *channel = getChannel(handle);
+	if (!channel || !channel->stream)
+		throw Common::Exception("Invalid channel");
+
+	channel->gain = gain;
+
+	if (_hasSound)
+		alSourcef(channel->source, AL_GAIN, _types[channel->type].gain * gain);
+}
+
+void SoundManager::setChannelPitch(const ChannelHandle &handle, float pitch) {
+	Common::StackLock lock(_mutex);
+
+	Channel *channel = getChannel(handle);
+	if (!channel || !channel->stream)
+		throw Common::Exception("Invalid channel");
+
+	if (_hasSound)
+		alSourcef(channel->source, AL_PITCH, pitch);
+}
+
+void SoundManager::setTypeGain(SoundType type, float gain) {
+	assert((type >= 0) && (type < kSoundTypeMAX));
+
+	Common::StackLock lock(_mutex);
+
+	// Set the new type gain
+	_types[type].gain = gain;
+
+	// Update all currently playing channels of that type
+	for (TypeList::iterator t = _types[type].list.begin(); t != _types[type].list.end(); ++t) {
+		assert(*t);
+
+		if (_hasSound)
+			alSourcef((*t)->source, AL_GAIN, (*t)->gain * gain);
+	}
+}
+
+bool SoundManager::fillBuffer(ALuint source, ALuint alBuffer, AudioStream *stream) const {
+	if (!stream)
+		throw Common::Exception("No stream");
+
+	if (!_hasSound)
+		return true;
+
+	if (stream->endOfData())
+		return false;
+
+	ALenum format;
+
+	const int channelCount = stream->getChannels();
+	if        (channelCount == 1) {
+		format = AL_FORMAT_MONO16;
+	} else if (channelCount == 2) {
+		format = AL_FORMAT_STEREO16;
+	} else if (channelCount == 6) {
+		if (!_hasMultiChannel) {
+			warning("SoundManager::fillBuffer(): TODO: !_hasMultiChannel");
+			return false;
+		}
+
+		format = _format51;
+
+	} else {
+		warning("SoundManager::fillBuffer(): Unsupported channel count %d", channelCount);
+		return false;
+	}
+
+	// Read in the required amount of samples
+	uint32 numSamples = kOpenALBufferSize / 2;
+
+	byte *buffer = new byte[kOpenALBufferSize];
+	memset(buffer, 0, kOpenALBufferSize);
+	numSamples = stream->readBuffer((int16 *)buffer, numSamples);
+
+	uint32 bufferSize = numSamples * 2;
+
+	alBufferData(alBuffer, format, buffer, bufferSize, stream->getRate());
+
+	delete[] buffer;
+
+	ALenum error = alGetError();
+	if (error != AL_NO_ERROR) {
+		warning("OpenAL error while filling buffer: 0x%X", error);
+		return false;
+	}
+
+	return true;
+}
+
+void SoundManager::bufferData(uint16 channel) {
+	if ((channel == 0) || !_channels[channel])
+		return;
+
+	bufferData(*_channels[channel]);
+}
+
+void SoundManager::bufferData(Channel &channel) {
+	if (!channel.stream || channel.stream->endOfData())
+		return;
+
+	if (!_hasSound)
+		return;
+
+	// Get the number of buffers that have been processed
+	ALint buffersProcessed;
+	alGetSourcei(channel.source, AL_BUFFERS_PROCESSED, &buffersProcessed);
+
+	// Pull all processed buffers from the queue and put them into our free list
+	while (buffersProcessed--) {
+		ALuint alBuffer;
+
+		alSourceUnqueueBuffers(channel.source, 1, &alBuffer);
+
+		channel.freeBuffers.push_back(alBuffer);
+	}
+
+	// Buffer as long as we still have data and free buffers
+	std::list<ALuint>::iterator buffer = channel.freeBuffers.begin();
+	while (buffer != channel.freeBuffers.end()) {
+		if (!fillBuffer(channel.source, *buffer, channel.stream))
+			break;
+
+		alSourceQueueBuffers(channel.source, 1, &*buffer);
+
+		buffer = channel.freeBuffers.erase(buffer);
+	}
+}
+
+void SoundManager::checkReady() {
+	if (!_ready)
+		throw Common::Exception("SoundManager not ready");
+}
+
+void SoundManager::update() {
+	Common::StackLock lock(_mutex);
+
+	for (int i = 1; i < kChannelCount; i++) {
+		if (!_channels[i])
+			continue;
+
+		// Free the channel if it is no longer playing
+		if (!isPlaying(i)) {
+			freeChannel(i);
+			continue;
+		}
+
+		// Try to buffer some more data
+		bufferData(i);
+	}
+}
+
+ChannelHandle SoundManager::newChannel() {
+	uint16 foundChannel = 0;
+
+	for (int i = 0; (i < kChannelCount) && !foundChannel; i++) {
+		if (!_channels[i])
+			foundChannel = i;
+
+		// Channel 0 is reserved for "invalid channel"
+		_curChannel = (_curChannel >= kChannelCount) ? 1 : (_curChannel + 1);
+	}
+
+	if (!foundChannel)
+		throw Common::Exception("All sound channels occupied");
+
+	ChannelHandle handle;
+
+	handle.channel = foundChannel;
+	handle.id      = _curID++;
+
+	// ID 0 is reserved for "invalid ID"
+	if (_curID == 0)
+		_curID++;
+
+	return handle;
+}
+
+void SoundManager::pauseChannel(Channel *channel, bool pause) {
+	if (!channel || channel->id == 0)
+		return;
+
+	ALenum error = AL_NO_ERROR;
+	if (pause) {
+		if (_hasSound) {
+			alSourcePause(channel->source);
+			if ((error = alGetError()) != AL_NO_ERROR)
+				warning("OpenAL error while attempting to pause: %X", error);
+		}
+
+		channel->state = AL_PAUSED;
+	} else
+		channel->state = AL_PLAYING;
+
+	triggerUpdate();
+}
+
+void SoundManager::pauseChannel(Channel *channel) {
+	if (!channel || channel->id == 0)
+		return;
+
+	if      (channel->state == AL_PAUSED)
+		pauseChannel(channel, false);
+	else if (channel->state == AL_PLAYING)
+		pauseChannel(channel, true);
+}
+
+void SoundManager::freeChannel(ChannelHandle &handle) {
+	if ((handle.channel != 0) && (handle.id != 0) && _channels[handle.channel])
+		// Only free if there is a channel to free
+		if (handle.id == _channels[handle.channel]->id)
+			// Only free if the IDs match
+			freeChannel(handle.channel);
+
+	handle.channel = 0;
+	handle.id      = 0;
+}
+
+void SoundManager::freeChannel(uint16 channel) {
+	if (channel == 0)
+		return;
+
+	Channel *c = _channels[channel];
+	if (!c)
+		// Nothing to do
+		return;
+
+	// Discard the stream, if requested
+	if (c->disposeAfterUse)
+		delete c->stream;
+
+	if (_hasSound) {
+		// Delete the channel's OpenAL source
+		if (c->source)
+			alDeleteSources(1, &c->source);
+
+		// Delete the OpenAL buffers
+		for (std::list<ALuint>::iterator buffer = c->buffers.begin(); buffer != c->buffers.end(); ++buffer)
+			alDeleteBuffers(1, &*buffer);
+	}
+
+	// Remove the channel from the type list
+	if (c->typeIt != _types[c->type].list.end())
+		_types[c->type].list.erase(c->typeIt);
+
+	// And finally delete the channel itself
+	delete c;
+	_channels[channel] = 0;
+}
+
+void SoundManager::threadMethod() {
+	while (!shouldQuitThread()) {
+		update();
+		_needUpdate.wait(100);
+	}
 }
 
 } // End of namespace Sound
--- src/sound/sound.h
+++ src/sound/sound.h
@@ -25,8 +25,22 @@
 #ifndef SOUND_SOUND_H
 #define SOUND_SOUND_H
 
+// Mac OS X has to have this set up separately because of the include
+// path for the OpenAL framework.
+#ifdef MACOSX
+	#include <OpenAL/al.h>
+	#include <OpenAL/alc.h>
+#else
+	#include <AL/al.h>
+	#include <AL/alc.h>
+#endif
+
 #include "common/types.h"
 #include "common/singleton.h"
+#include "common/thread.h"
+#include "common/mutex.h"
+
+#include "sound/types.h"
 
 namespace Common {
 	class SeekableReadStream;
@@ -37,12 +51,206 @@ namespace Sound {
 class AudioStream;
 
 /** The sound manager. */
-class SoundManager : public Common::Singleton<SoundManager> {
+class SoundManager : public Common::Singleton<SoundManager>, public Common::Thread {
 public:
 	SoundManager();
 	~SoundManager();
 
+	/** Initialize the sound subsystem. */
+	void init();
+	/** Deinitialize the sound subsystem. */
+	void deinit();
+
+	/** Was the sound subsystem successfully initialized? */
+	bool ready() const;
+
+
+	/** Signal that one of streams currently being played has changed and should be updated immediately. */
+	void triggerUpdate();
+
+
+	/** Does this channel handle point to an existing channel? */
+	bool isValidChannel(const ChannelHandle &handle) const;
+
+	/** Is that channel currently playing a sound? */
+	bool isPlaying(const ChannelHandle &handle);
+
+
+	// Playing sounds
+
+	/** Play a sound file.
+	 *
+	 *  This only allocate a channel for the sound, to actually start playing it,
+	 *  call startChannel().
+	 *
+	 *  @param  stream The data stream to play. Will be taken over.
+	 *  @param  type The type of the sound.
+	 *  @param  loop Should the sound loop?
+	 *  @return The channel the sound has been assigned to, or -1 on error.
+	 */
+	ChannelHandle playSoundFile(Common::SeekableReadStream *stream,
+	                            SoundType type, bool loop = false);
+
+	/** Play an audio stream.
+	 *
+	 *  This only allocate a channel for the sound, to actually start playing it,
+	 *  call startChannel().
+	 *
+	 *  @param  audStream The stream to play.
+	 *  @param  type The type of the sound.
+	 *  @param  disposeAfterUse Should the stream be taken over and discarded once it finshed?
+	 *  @return The channel the sound has been assigned to, or -1 on error.
+	 */
+	ChannelHandle playAudioStream(AudioStream *audStream,
+	                              SoundType type, bool disposeAfterUse = true);
+
+
+	// Starting/Pausing/Stopping channels
+
+	/** Start the channel. */
+	void startChannel(ChannelHandle &handle);
+
+	/** Pause the channel. */
+	void pauseChannel(ChannelHandle &handle, bool pause);
+
+	/** Pause toggle the channel. */
+	void pauseChannel(ChannelHandle &handle);
+
+	/** Stop and free the channel. */
+	void stopChannel(ChannelHandle &handle);
+
+
+	// Pausing/Stopping all channels
+
+	/** Pause all channels. */
+	void pauseAll(bool pause);
+
+	/** Stop all channels. */
+	void stopAll();
+
+
+	// Listener properties
+
+	/** Set the gain of the listener (= the global master volume). */
+	void setListenerGain(float gain);
+
+
+	// Channel properties
+
+	/** Set the position the channel is being played. */
+	void setChannelPosition(const ChannelHandle &handle, float x, float y, float z);
+
+	/** Get the position of the channel. */
+	void getChannelPosition(const ChannelHandle &handle, float &x, float &y, float &z);
+
+	/** Set the gain/volume of the channel. */
+	void setChannelGain(const ChannelHandle &handle, float gain);
+
+	/** Set the pitch of the channel. */
+	void setChannelPitch(const ChannelHandle &handle, float pitch);
+
+
+	// Type properties
+
+	/** Set the gain/volume of all channels of a specific type. */
+	void setTypeGain(SoundType type, float gain);
+
+
+	/** Create an audio stream from this data stream.
+	 *
+	 *  The ownership of the data stream is transferred to the audio stream
+	 *  if one was created without an exception being thrown.
+	 */
 	static AudioStream *makeAudioStream(Common::SeekableReadStream *stream);
+
+
+private:
+	static const int kChannelCount = 65535; ///< Maximal number of channels.
+
+	struct Channel;
+	typedef std::list<Channel *> TypeList;
+
+	/** A sound type. */
+	struct Type {
+		float    gain; ///< The sound type's current gain.
+		TypeList list; ///< The list of channels for that type.
+	};
+
+	/** A sound channel. */
+	struct Channel {
+		uint32 id; ///< The channel's ID.
+
+		ALint state; ///< The sound's state.
+
+		AudioStream *stream;  ///< The actual audio stream.
+		bool disposeAfterUse; ///< Delete the audio stream when done playing?
+
+		ALuint source; ///< OpenAL source for this channel.
+
+		std::list<ALuint> buffers;     ///< List of buffers for that channel.
+		std::list<ALuint> freeBuffers; ///< List of free buffers not filled with data.
+
+		SoundType type;            ///< The channel's sound type.
+		TypeList::iterator typeIt; ///< Iterator into the type list.
+
+		float gain; ///< The channel's gain.
+	};
+
+	bool _ready; ///< Was the sound subsystem successfully initialized?
+
+	bool _hasSound; //< Do we have working sound output?
+
+	bool _hasMultiChannel; ///< Do we have the multi-channel extension?
+	ALenum _format51; ///< The value for the 5.1 multi-channel format.
+
+	Channel *_channels[kChannelCount]; ///< The sound channels.
+	Type     _types   [kSoundTypeMAX]; ///< The sound types.
+
+	uint16 _curChannel; ///< Position to start looking for a free channel.
+	uint32 _curID;      ///< The ID the next sound will get.
+
+	Common::Mutex _mutex;
+
+	/** Condition to signal that an update is needed. */
+	Common::Condition _needUpdate;
+
+	ALCdevice *_dev;
+	ALCcontext *_ctx;
+
+	/** Check that the SoundManager was properly initialized. */
+	void checkReady();
+
+	/** Update the sound information. Called regularily from within the thread method. */
+	void update();
+
+	/** Look for a free place in the channel vector. */
+	ChannelHandle newChannel();
+
+	/** Buffer more sound from the channel to the OpenAL buffers. */
+	void bufferData(Channel &channel);
+	/** Buffer more sound from the channel to the OpenAL buffers. */
+	void bufferData(uint16 channel);
+
+	/** Is that channel currently playing a sound? */
+	bool isPlaying(uint16 channel) const;
+
+	/** Pause/Unpause a channel. */
+	void pauseChannel(Channel *channel, bool pause);
+	/** Pause toggle channel. */
+	void pauseChannel(Channel *channe);
+
+	/** Stop and free a channel. */
+	void freeChannel(ChannelHandle &handle);
+	/** Stop and free a channel. */
+	void freeChannel(uint16 channel);
+
+	/** Return the channel the handle refers to. */
+	Channel *getChannel(const ChannelHandle &handle);
+
+	void threadMethod();
+
+	/** Fill the buffer with data from the audio stream. */
+	bool fillBuffer(ALuint source, ALuint alBuffer, AudioStream *stream) const;
 };
 
 } // End of namespace Sound
