75f98543d9f59aa8cdfb3ea970527db6d8410e65
Sven Hesse <drmccoy@drmccoy.de>
2014-09-11 00:13:58 +0200

    IMAGES: Add TPC decoder from xoreos

---
+++ src/images/tpc.cpp
@@ -0,0 +1,225 @@
+/* Phaethon - A FLOSS resource explorer for BioWare's Aurora engine games
+ *
+ * Phaethon is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Phaethon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Phaethon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phaethon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file images/tpc.cpp
+ *  TPC (BioWare's own texture format) loading.
+ */
+
+#include "common/util.h"
+#include "common/maths.h"
+#include "common/error.h"
+#include "common/stream.h"
+
+#include "images/tpc.h"
+#include "images/util.h"
+
+static const byte kEncodingRGB          = 0x02;
+static const byte kEncodingRGBA         = 0x04;
+static const byte kEncodingSwizzledBGRA = 0x0C;
+
+namespace Images {
+
+TPC::TPC(Common::SeekableReadStream &tpc) : _txiData(0), _txiDataSize(0) {
+	load(tpc);
+}
+
+TPC::~TPC() {
+	delete[] _txiData;
+}
+
+void TPC::load(Common::SeekableReadStream &tpc) {
+	try {
+
+		bool needDeSwizzle = false;
+
+		readHeader (tpc, needDeSwizzle);
+		readData   (tpc, needDeSwizzle);
+		readTXIData(tpc);
+
+		if (tpc.err())
+			throw Common::Exception(Common::kReadError);
+
+	} catch (Common::Exception &e) {
+		e.add("Failed reading TPC file");
+		throw;
+	}
+}
+
+Common::SeekableReadStream *TPC::getTXI() const {
+	if (!_txiData || (_txiDataSize == 0))
+		return 0;
+
+	return new Common::MemoryReadStream(_txiData, _txiDataSize);
+}
+
+void TPC::readHeader(Common::SeekableReadStream &tpc, bool &needDeSwizzle) {
+	// Number of bytes for the pixel data in one full image
+	uint32 dataSize = tpc.readUint32LE();
+
+	tpc.skip(4); // Some float
+
+	// Image dimensions
+	uint32 width  = tpc.readUint16LE();
+	uint32 height = tpc.readUint16LE();
+
+	// How's the pixel data encoded?
+	byte encoding    = tpc.readByte();
+	// Number of mip maps in the image
+	byte mipMapCount = tpc.readByte();
+
+	tpc.skip(114); // Reserved
+
+	needDeSwizzle = false;
+
+	uint32 minDataSize = 0;
+	if (dataSize == 0) {
+		// Uncompressed
+
+		if        (encoding == kEncodingRGB) {
+			// RGB, no alpha channel
+
+			_format = kPixelFormatR8G8B8;
+
+			minDataSize = 3;
+			dataSize    = width * height * 3;
+
+		} else if (encoding == kEncodingRGBA) {
+			// RGBA, alpha channel
+
+			_format = kPixelFormatR8G8B8A8;
+
+			minDataSize = 4;
+			dataSize    = width * height * 4;
+
+		} else if (encoding == kEncodingSwizzledBGRA) {
+			// BGRA, alpha channel, texture memory layout is "swizzled"
+
+			needDeSwizzle = true;
+
+			_format = kPixelFormatB8G8R8A8;
+
+			minDataSize = 4;
+			dataSize    = width * height * 4;
+
+		} else
+			throw Common::Exception("Unknown TPC raw encoding: %d (%d)", encoding, dataSize);
+
+	} else if (encoding == kEncodingRGB) {
+		// S3TC DXT1
+
+		_format = kPixelFormatDXT1;
+
+		minDataSize = 8;
+
+	} else if (encoding == kEncodingRGBA) {
+		// S3TC DXT5
+
+		_format = kPixelFormatDXT5;
+
+		minDataSize = 16;
+
+	} else
+		throw Common::Exception("Unknown TPC encoding: %d (%d)", encoding, dataSize);
+
+	uint32 fullDataSize = tpc.size() - 128;
+
+	_mipMaps.reserve(mipMapCount);
+	for (uint32 i = 0; i < mipMapCount; i++) {
+		MipMap *mipMap = new MipMap;
+
+		mipMap->width  = MAX<uint32>(width,  1);
+		mipMap->height = MAX<uint32>(height, 1);
+
+		mipMap->size = MAX<uint32>(dataSize, minDataSize);
+
+		mipMap->data = 0;
+
+		if (fullDataSize < mipMap->size) {
+			// Wouldn't fit
+			delete mipMap;
+			break;
+		}
+
+		fullDataSize -= mipMap->size;
+
+		_mipMaps.push_back(mipMap);
+
+		width    >>= 1;
+		height   >>= 1;
+		dataSize >>= 2;
+
+		if ((width < 1) && (height < 1))
+			break;
+	}
+
+}
+
+void TPC::deSwizzle(byte *dst, const byte *src, uint32 width, uint32 height) {
+	for (uint32 y = 0; y < height; y++) {
+		for (uint32 x = 0; x < width; x++) {
+			const uint32 offset = deSwizzleOffset(x, y, width, height) * 4;
+
+			*dst++ = src[offset + 0];
+			*dst++ = src[offset + 1];
+			*dst++ = src[offset + 2];
+			*dst++ = src[offset + 3];
+		}
+	}
+}
+
+void TPC::readData(Common::SeekableReadStream &tpc, bool needDeSwizzle) {
+	for (std::vector<MipMap *>::iterator mipMap = _mipMaps.begin(); mipMap != _mipMaps.end(); ++mipMap) {
+
+		// If the texture width is a power of two, the texture memory layout is "swizzled"
+		const bool widthPOT = ((*mipMap)->width & ((*mipMap)->width - 1)) == 0;
+		const bool swizzled = needDeSwizzle && widthPOT;
+
+		(*mipMap)->data = new byte[(*mipMap)->size];
+
+		if (swizzled) {
+			std::vector<byte> tmp((*mipMap)->size);
+
+			if (tpc.read(&tmp[0], (*mipMap)->size) != (*mipMap)->size)
+				throw Common::Exception(Common::kReadError);
+
+			deSwizzle((*mipMap)->data, &tmp[0], (*mipMap)->width, (*mipMap)->height);
+
+		} else {
+			if (tpc.read((*mipMap)->data, (*mipMap)->size) != (*mipMap)->size)
+				throw Common::Exception(Common::kReadError);
+		}
+
+	}
+}
+
+void TPC::readTXIData(Common::SeekableReadStream &tpc) {
+	// TXI data for the rest of the TPC
+	_txiDataSize = tpc.size() - tpc.pos();
+
+	if (_txiDataSize == 0)
+		return;
+
+	_txiData = new byte[_txiDataSize];
+
+	if (tpc.read(_txiData, _txiDataSize) != _txiDataSize)
+		throw Common::Exception(Common::kReadError);
+}
+
+} // End of namespace Images
