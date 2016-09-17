383146ab907b1b731052cf2435f4cb1b2dddb480
Sven Hesse <drmccoy@drmccoy.de>
2016-09-17 19:21:18 +0200

    TESTS: Add unit tests for our utility templates and functions

---
+++ tests/version/version.cpp
@@ -0,0 +1,51 @@
+/* Phaethon - A FLOSS resource explorer for BioWare's Aurora engine games
+ *
+ * Phaethon is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Phaethon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Phaethon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phaethon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Unit test mock of src/version/version.cpp.
+ */
+
+namespace Version {
+
+const char *getProjectName() {
+	return "";
+}
+
+const char *getProjectVersion() {
+	return "";
+}
+
+const char *getProjectNameVersion() {
+	return "";
+}
+
+const char *getProjectNameVersionFull() {
+	return "";
+}
+
+const char *getProjectURL() {
+	return "";
+}
+
+const char *getProjectAuthors() {
+	return "";
+}
+
+} // End of namespace Version
