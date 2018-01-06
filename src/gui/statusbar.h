d487f289a8c43fea202066fed79ce1a6238cb817
fdde <fdde00@gmail.com>
2018-01-06 19:32:21 +0000

    GUI: Add license to files

--- src/gui/statusbar.h
+++ src/gui/statusbar.h
@@ -1,3 +1,28 @@
+/* Phaethon - A FLOSS resource explorer for BioWare's Aurora engine games
+ *
+ * Phaethon is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Phaethon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Phaethon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phaethon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Wrapper class for status bar in order to
+ *  have identical functionality to Phaethon (wx version.)
+ */
+
 #ifndef STATUSBAR_H
 #define STATUSBAR_H
 
@@ -5,18 +30,9 @@
 #include <QStatusBar>
 #include <QWidget>
 
-/** @file
- *  Wrapper class for status bar in order to
- *  have identical functionality to Phaethon (wx version.)
- */
-
 namespace GUI {
 
-/** Class to have identical functionality to Phaethon.
- *  Or wx.                                              **/
-
-class StatusBar
-{
+class StatusBar {
 public:
     StatusBar(QStatusBar *statusBar);
     void setText(const QString &text);
