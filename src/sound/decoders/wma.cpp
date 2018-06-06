xoreos/phaethon

commit 7303d37d6aa3e01712d6b0bf4cb56780e95310d5

SOUND: Use the PHAETHON_FALLTHROUGH macro to mark fallthroughs

src/sound/decoders/wma.cpp
@@ -1244,10 +1244,10 @@ bool WMACodec::decodeExpHuffman(Common::BitStream &bits, int ch) {
 		int n = *ptr++;
 
 		switch (n & 3) do {
-			case 0: *q++ = iv; // Fallthrough
-			case 3: *q++ = iv; // Fallthrough
-			case 2: *q++ = iv; // Fallthrough
-			case 1: *q++ = iv;
+			case 0: *q++ = iv; PHAETHON_FALLTHROUGH;
+			case 3: *q++ = iv; PHAETHON_FALLTHROUGH;
+			case 2: *q++ = iv; PHAETHON_FALLTHROUGH;
+			case 1: *q++ = iv; break;
 		} while ((n -= 4) > 0);
 
 	} else
@@ -1276,10 +1276,10 @@ bool WMACodec::decodeExpHuffman(Common::BitStream &bits, int ch) {
 		int n = *ptr++;
 
 		switch (n & 3) do {
-			case 0: *q++ = iv; // Fallthrough
-			case 3: *q++ = iv; // Fallthrough
-			case 2: *q++ = iv; // Fallthrough
-			case 1: *q++ = iv;
+			case 0: *q++ = iv; PHAETHON_FALLTHROUGH;
+			case 3: *q++ = iv; PHAETHON_FALLTHROUGH;
+			case 2: *q++ = iv; PHAETHON_FALLTHROUGH;
+			case 1: *q++ = iv; break;
 		} while ((n -= 4) > 0);
 
 	}

commit 335fa297eb15793262a77bf19e9c3b64fa5cf473

SOUND: Fix WMA decoding broken in 7303d37d6
Because of Duff's Device, the break would be for the loop, not for the
switch. Putting a break there to shut the compiler up about the
fall-through is wrong. Instead, a fall-through annotation needs to go
before the first case.

src/sound/decoders/wma.cpp
@@ -1244,10 +1244,11 @@ bool WMACodec::decodeExpHuffman(Common::BitStream &bits, int ch) {
 		int n = *ptr++;
 
 		switch (n & 3) do {
+			PHAETHON_FALLTHROUGH;
 			case 0: *q++ = iv; PHAETHON_FALLTHROUGH;
 			case 3: *q++ = iv; PHAETHON_FALLTHROUGH;
 			case 2: *q++ = iv; PHAETHON_FALLTHROUGH;
-			case 1: *q++ = iv; break;
+			case 1: *q++ = iv;
 		} while ((n -= 4) > 0);
 
 	} else
@@ -1276,10 +1277,11 @@ bool WMACodec::decodeExpHuffman(Common::BitStream &bits, int ch) {
 		int n = *ptr++;
 
 		switch (n & 3) do {
+			PHAETHON_FALLTHROUGH;
 			case 0: *q++ = iv; PHAETHON_FALLTHROUGH;
 			case 3: *q++ = iv; PHAETHON_FALLTHROUGH;
 			case 2: *q++ = iv; PHAETHON_FALLTHROUGH;
-			case 1: *q++ = iv; break;
+			case 1: *q++ = iv;
 		} while ((n -= 4) > 0);
 
 	}